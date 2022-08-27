0290a99f00a415c7ccf62cae2022a6f77d47d31f
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-08-27 18:21:09 -0300

    Atualização

---
+++ lib/delegates/data_search.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate<String> {
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {},
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    throw UnimplementedError();
+  }
+
+
+
+}
